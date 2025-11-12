3aaa8c60d672412486276b01c562138b46b1dd72
Kirill <publickirill2245@gmail.com>
2025-11-12 05:50:16 +0500

    Full craete section Hero in section Home

--- src/components/MainSections/Home/Hero/HeroInfo/HeroInfo.vue
+++ src/components/MainSections/Home/Hero/HeroInfo/HeroInfo.vue
@@ -1,15 +1,18 @@
 <template>
-    <section class="flex w-full bg-grey-08 gap-20">
+    <section class="flex w-full bg-grey-08 gap-20 relative">
         <InfoContain/>
         <img :src="img" alt="ilustration" aria-hidden="true" class="h-203.5 flex-1 max-w-1/2 object-cover"></img>
+        <SubContiner/>
     </section>
 </template>=
 <script lang="ts">
     import image from '../../../../../assets/Container.jpg'
     import InfoContain from './InfoContain/InfoContain.vue';
+    import SubContiner from './SubContiner.vue';
     export default{
         components:{
-            InfoContain
+            InfoContain,
+            SubContiner
         },
         data(){
             return {
---
+++ src/components/MainSections/Home/Hero/HeroInfo/SubContiner.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="flex bg-grey-08 w-43.75 h-43.75 absolute top-36 left-8/17 rounded-[50%] border border-solid border-grey-15 items-center justify-center">
+            <svg class="absolute inset-0 w-full h-full" viewBox="0 0 200 200">
+            <defs>
+                <path id="circlePath" d="M 100, 100 m -75, 0 a 75,75 0 1,1 150,0 a 75,75 0 1,1 -150,0" />
+            </defs>
+            <text fill="white" font-size="18"  font-weight="600" letter-spacing="6">
+                <textPath href="#circlePath" startOffset="0%">
+                Discover Your Dream Property ✨ 
+                </textPath>
+            </text>
+            </svg>
+        <div class="w-20 h-20 bg-grey-10 border border-solid border-grey-15  rounded-[50%] flex items-center justify-center">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1 22.25L22.25 1M22.25 1L6.3125 1M22.25 1V16.9375" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+        </div>
+    </div>
+</template>=
+<script lang="ts">
+
+    export default{
+
+    }
+</script>
